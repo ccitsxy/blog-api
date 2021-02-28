ca5d6db4e6df9cb56ec071b40b533ab33e3b1902
11278 <1127885451@qq.com>
2021-02-28 19:07:18 +0800

    Update ArticleController.java

--- src/main/java/com/sxy/blog/controller/ArticleController.java
+++ src/main/java/com/sxy/blog/controller/ArticleController.java
@@ -5,17 +5,27 @@ import com.sxy.blog.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/articles")
 public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @GetMapping("/findAll")
-    public Page<Article> findAll(@RequestParam("pageNum") int pageNum, @RequestParam("size") int size) {
+    @GetMapping("/")
+    public Page<Article> findAll(@RequestParam("pageNum") Integer pageNum,
+                                 @RequestParam("size") Integer size) {
         return articleService.findAll(PageRequest.of(pageNum, size));
+    }
+
+    @PostMapping("/")
+    public void saveArticle(@RequestParam("title") String title,
+                            @RequestParam("markdown") String Markdown,
+                            @RequestParam("html") String html) {
+        Article article = new Article();
+        article.setTitle("");
+        article.setMarkdown("");
+        article.setHtml("");
     }
 }
