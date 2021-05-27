58e2ff97771ae094e2159c80b2793f81b1f00568
11278 <1127885451@qq.com>
2021-05-27 21:33:36 +0800

    update

--- src/main/java/com/sxy/blog/entity/Comment.java
+++ src/main/java/com/sxy/blog/entity/Comment.java
@@ -8,19 +8,37 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "article"})
 @Entity
 @Table(name = "comment")
 public class Comment extends Base {
-    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     @Column(name = "coid")
-    private Long coid;
+    private Integer coid;
 
-    
+    @Column(name = "author_id")
+    private Integer authorId;
+
+    @Column(name = "text")
+    private String text;
+
+    @JoinColumn(name = "aid", referencedColumnName = "aid")
+    @ManyToOne(targetEntity = Article.class, fetch = FetchType.EAGER)
+    private Article article;
+
+    @JsonIgnoreProperties({"parent"})
+    @ManyToOne(targetEntity = Comment.class, fetch = FetchType.EAGER)
+    private Comment parent;
+
+    @JsonIgnoreProperties({"parent"})
+    @OneToMany(mappedBy = "parent", fetch = FetchType.EAGER)
+    private Set<Comment> children = new LinkedHashSet<>();
 }
